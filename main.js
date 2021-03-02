cfa0a2b660637852bf0c0c6fb120c1e95c967891
varun2010 <76247848+varun2010@users.noreply.github.com>
2021-03-02 13:38:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+Webcam.set({
+    height:300,
+    width:310,
+    image_format:"png",
+    png_quality:90,
+    constraints:{
+        facingMode:"environment"
+    }
+});
+camera=document.getElementById("camera");
+Webcam.attach(camera);
